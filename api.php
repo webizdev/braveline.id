6ddf806eb2b682ee8396a77c85e11e4382566b9e
webizdev <arifinamruna2@gmail.com>
2026-05-05 05:11:49 +0800

    Update project files: index, admin, api, and docker configs

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header(\"Access-Control-Allow-Origin: *\");
+header(\"Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS\");
+header(\"Access-Control-Allow-Headers: Content-Type\");
+header(\"Content-Type: application/json\");
 
 // Database Configuration
 $host = 'localhost';
